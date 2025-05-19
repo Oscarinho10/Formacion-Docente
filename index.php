b46658eb3aa06388b44da1dbe8ceb6818b10dc54
Oscarinho10 <20233tn143@utez.edu.mx>
2025-05-19 14:02:17 -0600

    Segundo commit Gio

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+// index.php
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Formación Docente</title>
+    <link rel="stylesheet" type="text/css" href="css/estilo.css">
+</head>
+
+<body>
+    <div class="sidebar">
+        <div class="logo">
+            <img src="./img/logo_blanco.png">
+        </div>
+        <ul>
+            <li><a href="#">Perfil</a></li>
+            <li><a href="#">Solicitudes</a></li>
+            <li><a href="#">Actividad formativa</a></li>
+            <li><a href="#">Asistencias</a></li>
+            <li><a href="#">Constancias</a></li>
+            <li><a href="#">Reportes</a></li>
+            <li><a href="#">Cerrar sesión</a></li>
+        </ul>
+    </div>
+
+    <div class="main-content">
+        <h1>Universidad Autónoma del Estado de Morelos</h1>
+        <h3>Secretaría Académica</h3>
+        <h4>Dirección General de Educación Superior</h4>
+        <h4>Departamento de Evaluación y Profesionalización de la Docencia</h4>
+    </div>
+
+</body>
+
+</html>
