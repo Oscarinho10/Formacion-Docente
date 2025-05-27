785d3904ea9b0eef4563941cabb2cc6ff9a2f7b8
AngelAkagami <angel1421nl@gmail.com>
2025-05-27 11:39:29 -0600

    Cambio de rutas y carpetas

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 // Define la ruta base del proyecto
-define('RUTA_BASE', '/Formacion-Docente');
+define('RUTA_BASE', '/formacion/PROYECTO/Formacion-Docente');
 
 // Define la URL base completa
 define('BASE_URL', 'http://' . $_SERVER['HTTP_HOST'] . RUTA_BASE);
