4d2513f1a08cf3a60fd5c9e83f8acecedf0e8145
Oscarinho10 <20233tn143@utez.edu.mx>
2025-07-09 13:34:55 -0600

    subo cambios de catalogo

--- config/controller/catalogoController.php
+++ config/controller/catalogoController.php
@@ -1,6 +1,6 @@
 <?php
 // Consulta solo las actividades activas
-$query = "SELECT * FROM actividades_formativas WHERE estado = 'activa'";
+$query = "SELECT * FROM actividades_formativas WHERE estado = 'activo'";
 $resultado = pg_query($conn, $query);
 
 // Verificamos si hay resultados
