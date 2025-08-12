5dfb157f3367a15eaadc3742e7cca600988db365
Oscarinho10 <20233tn143@utez.edu.mx>
2025-08-12 14:15:07 -0600

    ip

--- config/conexion.php
+++ config/conexion.php
@@ -1,9 +1,9 @@
 <?php
-$host = "172.21.37.83"; // 172.21.37.83 "Gio uaem" ;  // 172.21.37.84 "Ángel uaem" //Casa de Gio 192.168.100.28
+$host = "148.218.78.37"; // 172.21.37.83 "Gio uaem" ;  // 172.21.37.84 "Ángel uaem" //Casa de Gio 192.168.100.28
 $port = "5432";
 $dbname = "formacion_docente2";
 $user = "jerss";
-$password = "admin";
+$password = "root";
 
 $conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 
